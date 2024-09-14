8fa65f225446b1f871cec078d57c77df1c948d1b
Cheney <gary21978@126.com>
2024-09-14 17:01:38 +0800

    Remove twice transpose operation

--- CalculateAerialImage_SOCS.py
+++ CalculateAerialImage_SOCS.py
@@ -2,19 +2,15 @@ import torch
 
 def CalculateAerialImage_SOCS(scatter, TCCMatrix_SOCS, source, projector):
     spectrum, f, g, pupilImage = scatter.CalculateSpectrum(projector, source)
-    intensity = 0
-    for channel in range(3):
-        spectrum_channel = spectrum[:, :, channel].squeeze()
-        temp = TCCMatrix_SOCS * torch.fft.fftshift(spectrum_channel).unsqueeze(2)
-        temp = temp.permute(2, 1, 0)
-        Etemp = (f[1] - f[0]) * (g[1] - g[0]) * torch.fft.fft2(temp)
-        Esquare = torch.abs(Etemp)**2
-        intensity_channel = torch.sum(Esquare, dim=0)
-        intensity_channel = torch.fft.fftshift(intensity_channel)
-        intensity_channel = torch.rot90(intensity_channel, 2)
-        intensity_channel = torch.transpose(intensity_channel, -2, -1)
-        intensity = intensity + intensity_channel
+    sp = spectrum.unsqueeze(3).permute(0, 1, 3, 2)
+    Es = TCCMatrix_SOCS.unsqueeze(3) * torch.fft.fftshift(sp, dim=(0, 1))
+    E = (f[1] - f[0]) * (g[1] - g[0]) * torch.fft.fft2(Es, dim=(0, 1))
+    Esquare = torch.abs(E)**2
+    intensity = torch.sum(Esquare, dim=(2, 3)) # sum over xyz and coherent systems
+    intensity = torch.fft.fftshift(intensity, dim=(0, 1))
+    intensity = torch.rot90(intensity, 2)
     return intensity, pupilImage
+    
 def CalculateSOCS(FloquetMode, TCCMatrix_SOCS, numerics):
     n_scatter_x = numerics.ScatterGrid_X
     n_scatter_y = numerics.ScatterGrid_Y
