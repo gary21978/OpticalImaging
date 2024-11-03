Fixed computation

rcwa.py
@@ -137,7 +137,6 @@ class rcwa:
         self._solve_layer_smatrix()
 
     def add_defect(self, layer_num=0, deps=None):
-        #self.eps_conv[layer_num] += self._material_conv(deps)
         deps_conv = self._material_conv(deps)
         self._update_layer_smatrix(layer_num, deps_conv)
 
@@ -450,10 +449,8 @@ class rcwa:
         # baseline states
         E0 = self.eps_conv[layer_num]
         Q0 = self.Q[layer_num]
-        PQ0 = self.PQ[layer_num]
         W0 = self.E_eigvec[layer_num]
-        V0 = self.H_eigvec[layer_num]
-        D0 = self.kz_norm[layer_num]
+        Kz0 = self.kz_norm[layer_num]
         E0i = torch.linalg.inv(E0)
         W0i = torch.linalg.inv(W0)
         Kx = self.Kx_norm
@@ -471,19 +468,17 @@ class rcwa:
 
         Q = Q0 + Qd
 
-        D0sq = D0**2
-        phi = D0sq.unsqueeze(-1) - D0sq.unsqueeze(-2)
+        Kz0sq = Kz0**2
+        phi = Kz0sq.unsqueeze(1) - Kz0sq.unsqueeze(0)
         phi = torch.where(phi == 0.0, 0.0, 1.0/phi)
         WdW = W0i@PQd@W0
-        Dd = torch.diagonal(WdW)*torch.where(D0 == 0.0, 0.0, 0.5/D0)
+        Kzd = torch.diagonal(WdW)*torch.where(Kz0 == 0.0, 0.0, 0.5/Kz0)
         Wd = -W0@(phi*WdW)
-        Vd = Q@Wd@torch.linalg.inv(torch.diag(D0))
 
-        kz_norm = D0 + Dd
+        kz_norm = Kz0 + Kzd
         E_eigvec = W0 + Wd
-        H_eigvec = V0 + Vd
         
-        #H_eigvec = Q@E_eigvec@torch.linalg.inv(torch.diag(kz_norm))
+        H_eigvec = Q@E_eigvec@torch.linalg.inv(torch.diag(kz_norm))
         self.kz_norm[layer_num] = kz_norm
         self.E_eigvec[layer_num] = E_eigvec
         self.H_eigvec[layer_num] = H_eigvec
@@ -498,13 +493,6 @@ class rcwa:
         C2 = -C1@BAi
         S11 = W@((phase@C1) + C2)
         S12 = W@((phase@C2) + C1) - torch.eye(2*self.order_N,dtype=self._dtype,device=self._device)
-
-        #V0iV0 = torch.linalg.inv(self.Vf)@V0
-        #V0iVd = torch.linalg.inv(self.Vf)@Vd
-        #phase0 = torch.diag(torch.exp(1.j*self.omega*D0*self.thickness[layer_num]))
-        #phased = torch.diag(1.j*self.omega*Dd*self.thickness[layer_num])*phase0
-        #Ad = Wd + V0iVd
-        #Bd = (Wd - V0iVd)@phase0 + (W0 - V0iV0)@phased
 
         self.layer_S11[layer_num] = S11
         self.layer_S12[layer_num] = S12